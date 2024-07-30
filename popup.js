1810e36dbb54ce76f5fd9e27d7998f08abbc2252
szriru <szriko.abyss@gmail.com>
2024-07-31 08:24:49 +0900

    feat: file format selection

--- popup.js
+++ popup.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const voiceList = document.getElementById('voice-list');
   const fileFormatList = document.getElementById('file-format-list');
 
+  chrome.storage.local.get(['isFirstRun'], function(result) {
+    if (result.isFirstRun) {
+      chrome.storage.local.set({
+        fileFormat: fileFormatList.options[0].value,
+        selectedVoice: voiceList.options[0].value,
+        isFirstRun: false
+      });
+      fileFormatList.value = fileFormatList.options[0].value;
+      voiceList.value = voiceList.options[0].value;
+    }
+  });
+  
   chrome.storage.local.get(['fileFormat'], function(result) {
     if (result.fileFormat) {
       fileFormatList.value = result.fileFormat;
