26a40fd96770ba15f8503a2c69e6e6529ec144f6
szriru <szriko.abyss@gmail.com>
2024-07-31 08:20:47 +0900

    feat: file format selection

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({ isFirstRun: true });
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch (message.action) {
     case "saveVoiceSelection":
